34e97ab0b3eea5c58ea8589858aaa6f31f664697
bhandari8sujit <bhandari8.sujit@gmail.com>
2018-05-10 11:54:11 +0545

    model

--- dev/scripts/vendors/file.js
+++ dev/scripts/vendors/file.js
@@ -1,35 +1,39 @@
 $(document).ready(function(){
     
-    $('.owl-1').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            768:{
-                items:2,
-                nav:false
-            },
-            992:{
-                items:3,
-                nav:true,
-                
-            },
-            1200:{
-                items:6,
-                nav:true,
-                loop:false
-            },
-            1800:{
-                items:5,
-                nav:true,
-                loop:false
-            }
-        }
-    })
+    //$('.owl-1').owlCarousel({
+    //    loop:true,
+    //    margin:10,
+    //    responsiveClass:true,
+    //    responsive:{
+    //        0:{
+    //            items:1,
+    //            nav:true
+    //        },
+    //        500:{
+    //            items:2,
+    //            nav:true
+    //        },
+    //        768:{
+    //            items:2,
+    //            nav:false
+    //        },
+    //        992:{
+    //            items:3,
+    //            nav:true,
+    //            
+    //        },
+    //        1200:{
+    //            items:6,
+    //            nav:true,
+    //            loop:false
+    //        },
+    //        1800:{
+    //            items:6,
+    //            nav:true,
+    //            loop:false
+    //        }
+    //    }
+    //})
 
     $('.owl-2').owlCarousel({
         loop:true,
